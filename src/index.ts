e01b9fadbf6885828540014231fa1823280f06ba
Tomotada Saigusa <nirot1r@g-second.net>
2020-01-21 18:18:17 +0900

    環境移動

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,6 @@ import Discord from 'discord.js';
 
 import dateH from './libs/date';
 
-import pkg from './../package.json';
-
 require('date-utils');
 require('dotenv').config();
 
@@ -27,63 +25,113 @@ const intensityColor = {
   '7': 'd166ca'
 };
 
+class EV1 {
+  private token: string;
+  private bot: Discord.Client;
+  private nied: NIED;
+  private nhk: NHK;
+
+  constructor(token: string | undefined) {
+    if (!token) {
+      throw new ReferenceError('トークンが入力されていません。');
+    }
+    this.token = token;
+
+    this.bot = new Discord.Client();
+    this.nied = new NIED();
+    this.nhk = new NHK();
+  }
+
+  start(): void {
+    this.bot.login(this.token);
+    this.nied.start();
+    this.nhk.start();
+  }
+
+  private niedReadyHandler(): void {
+    this.nied.on('ready', () =>
+      console.log('✔ NIEDクライアントの準備が整いました！')
+    );
+  }
+
+  private nhkReadyHandler(): void {
+    this.nhk.on('ready', () =>
+      console.log('✔ NHKクライアントの準備が整いました！')
+    );
+  }
+
+  private niedHandler(): void {
+    this.nied.on('data', (data: NIED.Earthquake) => {
+      console.log(
+        `<i> NIED地震速報が届きました ${data.report_id}-${data.report_num}`
+      );
+
+      const ch: Discord.Channel | undefined = this.bot.channels.get(
+        '651780233711583233'
+      );
+      if (!(ch instanceof Discord.TextChannel)) return;
+
+      ch.send({
+        embed: {
+          title: `地震速報(高度利用) 第${data.report_num}報${
+            data.is_final ? ' (最終報)' : ''
+          }`,
+          color: parseInt(
+            `0x${intensityColor[data.calcintensity] || '888888'}`,
+            16
+          ),
+          thumbnail: {
+            url: `https://github.com/neirowork/EV1/blob/develop/assets/intensity/${encodeURIComponent(
+              data.calcintensity
+            )}.png?raw=true`
+          },
+          fields: [
+            {
+              name: '発生時刻',
+              value: dateH.datetimeToDate(data.origin_time),
+              inline: true
+            },
+            { name: '震央', value: data.region_name, inline: true },
+            { name: '深さ', value: data.depth, inline: true },
+            {
+              name: 'マグニチュード',
+              value: `M${data.magunitude}`,
+              inline: true
+            },
+            {
+              name: '予想最大震度',
+              value: `震度${data.calcintensity}`,
+              inline: true
+            }
+          ],
+          author: {
+            name: '情報元 : NIED 防災科学技術研究所',
+            url: 'http://www.kmoni.bosai.go.jp/',
+            icon_url:
+              'https://github.com/neirowork/EV1/blob/develop/assets/nied.png?raw=true'
+          },
+          footer: {
+            text: 'NeiRo.WORK Earthquake Viewer 1'
+            // icon_url: ''
+          }
+        }
+      });
+    });
+  }
+}
+
+const ev1 = new EV1(process.env.TOKEN);
+ev1.start();
+
 client.on('ready', () => {
   console.log('✔ Discordクライアントの準備が整いました！');
-  client.user.setActivity(`EV1 v${pkg.version}`, {
+  client.user.setActivity(`EV1 v${process.env.npm_package_version}`, {
     url: 'https://github.com/neirowork/EV1',
     type: 'PLAYING'
   });
 });
 
 nied.on('ready', () => console.log('✔ NIEDクライアントの準備が整いました！'));
-nhk.on('ready', () => console.log('✔ NHKクライアントの準備が整いました！'));
-
-nied.on('data', data => {
-  console.log(
-    `<i> NIED地震速報が届きました ${data.report_id}-${data.report_num}`
-  );
-  client.channels.get('651780233711583233').send({
-    embed: {
-      title: `地震速報(高度利用) 第${data.report_num}報${
-        data.is_final ? ' (最終報)' : ''
-      }`,
-      color: parseInt(
-        `0x${intensityColor[data.calcintensity] || '888888'}`,
-        16
-      ),
-      thumbnail: {
-        url: `https://github.com/neirowork/EV1/blob/develop/assets/intensity/${encodeURIComponent(
-          data.calcintensity
-        )}.png?raw=true`
-      },
-      fields: [
-        {
-          name: '発生時刻',
-          value: dateH.datetimeToDate(data.origin_time),
-          inline: true
-        },
-        { name: '震央', value: data.region_name, inline: true },
-        { name: '深さ', value: data.depth, inline: true },
-        { name: 'マグニチュード', value: `M${data.magunitude}`, inline: true },
-        {
-          name: '予想最大震度',
-          value: `震度${data.calcintensity}`,
-          inline: true
-        }
-      ],
-      author: {
-        name: '情報元 : NIED 防災科学技術研究所',
-        url: 'http://www.kmoni.bosai.go.jp/',
-        icon_url:
-          'https://github.com/neirowork/EV1/blob/develop/assets/nied.png?raw=true'
-      },
-      footer: {
-        text: 'NeiRo.WORK Earthquake Viewer 1'
-        // icon_url: ''
-      }
-    }
-  });
-});
 
 nhk.on('data', data => {
   console.log(`<i> NHK地震情報が届きました ${data.id}`);
